16300faec20b18d97b2141fea2ee00f16384cbf1
Jeff Fontenot <jeff@itodyssey.io>
2026-09-12 22:28:53 -0500

    feat: link command events to active sessions

--- cmd/record.go
+++ cmd/record.go
@@ -4,6 +4,7 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/it-odyssey/waketrail/internal/state"
 	"github.com/it-odyssey/waketrail/internal/storage"
 	"github.com/spf13/cobra"
 )
@@ -20,6 +21,7 @@ var recordCmd = &cobra.Command{
 	Short:  "Record a shell command",
 	Hidden: true,
 	Args:   cobra.ExactArgs(1),
+
 	RunE: func(cmd *cobra.Command, args []string) error {
 		store, err := storage.Open()
 		if err != nil {
@@ -27,7 +29,24 @@ var recordCmd = &cobra.Command{
 		}
 		defer store.Close()
 
+		var sessionID *int64
+
+		active, err := state.HasActiveSession()
+		if err != nil {
+			return err
+		}
+
+		if active {
+			session, err := state.LoadSession()
+			if err != nil {
+				return err
+			}
+
+			sessionID = &session.ID
+		}
+
 		event := storage.CommandEvent{
+			SessionID: sessionID,
 			Command:   args[0],
 			Cwd:       recordCwd,
 			ExitCode:  recordExitCode,
@@ -44,28 +63,47 @@ var recordCmd = &cobra.Command{
 }
 
 func init() {
-	recordCmd.Flags().StringVar(&recordCwd, "cwd", "", "Working directory")
-	recordCmd.Flags().IntVar(&recordExitCode, "exit-code", 0, "Command exit code")
+	recordCmd.Flags().StringVar(
+		&recordCwd,
+		"cwd",
+		"",
+		"Working directory",
+	)
 
-	recordCmd.Flags().Func("started-at", "Command start time in Unix nanoseconds", func(value string) error {
-		parsed, err := strconv.ParseInt(value, 10, 64)
-		if err != nil {
-			return err
-		}
+	recordCmd.Flags().IntVar(
+		&recordExitCode,
+		"exit-code",
+		0,
+		"Command exit code",
+	)
 
-		recordStartedAt = parsed
-		return nil
-	})
+	recordCmd.Flags().Func(
+		"started-at",
+		"Command start time in Unix nanoseconds",
+		func(value string) error {
+			parsed, err := strconv.ParseInt(value, 10, 64)
+			if err != nil {
+				return err
+			}
 
-	recordCmd.Flags().Func("ended-at", "Command end time in Unix nanoseconds", func(value string) error {
-		parsed, err := strconv.ParseInt(value, 10, 64)
-		if err != nil {
-			return err
-		}
+			recordStartedAt = parsed
+			return nil
+		},
+	)
 
-		recordEndedAt = parsed
-		return nil
-	})
+	recordCmd.Flags().Func(
+		"ended-at",
+		"Command end time in Unix nanoseconds",
+		func(value string) error {
+			parsed, err := strconv.ParseInt(value, 10, 64)
+			if err != nil {
+				return err
+			}
+
+			recordEndedAt = parsed
+			return nil
+		},
+	)
 
 	rootCmd.AddCommand(recordCmd)
 }
